a5437c2693ff51ad1c0fa50b334e3412cc12ac49
AaronJan <aaronjan@qq.com>
2015-06-10 16:41:16 +0800

    set up things for UI developing

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,37 @@
+/**
+ * Gulp tasks
+ */
+'use strict';
+
 var elixir = require('laravel-elixir');
+var browserSync = require('browser-sync');
+var gulp = require('gulp');
+var express = require('express');
+var morgan = require('morgan');
+
+gulp.task('server', function () {
+    var server = express();
+
+    server.use(morgan('dev'));
+    server.use(express.static(__dirname + '/public/html'));
+
+    server.listen(9001);
+});
+
+gulp.task('browserSync', function () {
+    browserSync({
+        proxy : 'localhost:9001',
+        notify: false
+    });
+});
+
+gulp.task('reload', function () {
+    browserSync.reload();
+});
+
+gulp.task('watch', ['browserSync', 'server'], function () {
+    gulp.watch(__dirname + '/public/html/*', ['reload']);
+});
 
 /*
  |--------------------------------------------------------------------------
@@ -11,6 +44,11 @@ var elixir = require('laravel-elixir');
  |
  */
 
-elixir(function(mix) {
-    mix.less('app.less');
+gulp.task('elixir', function () {
+    elixir(function (mix) {
+        mix.less('app.less');
+    });
 });
+
+
+gulp.task('default', ['watch']);
